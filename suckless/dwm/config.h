9062e374a43358d35ac0590e5c90560e43da7189
Santhosh Sundaram <santhoshsundaram9650@gmail.com>
2024-02-20 19:02:58 +0530

    added rofi command to dwm

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -63,8 +63,8 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "terminator", NULL };
-
+static const char *termcmd[]  = { "alacritty", NULL };
+static const char *roficmd[] = {"rofi", "-show", "drun"}; 
 static const char *mutecmd[] = { "pactl", "set-sink-mute", "0", "toggle", NULL };
 static const char *volupcmd[] = { "pactl", "set-sink-volume", "0", "+5%", NULL };
 static const char *voldowncmd[] = { "pactl", "set-sink-volume", "0", "-5%", NULL };
@@ -73,6 +73,7 @@ static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY, 	            	XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY, 	            	XK_r,      spawn,          {.v = roficmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
