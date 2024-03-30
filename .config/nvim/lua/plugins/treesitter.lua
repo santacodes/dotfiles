6b6c0bbf38e883e401f7a1664686a1adfef96631
Santhosh Sundaram <santhoshsundaram9650@gmail.com>
2024-03-30 23:04:02 +0530

    lsp and statusbar changes

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,18 +1,39 @@
 local M = {
-    "nvim-treesitter/nvim-treesitter",
-    build = function()
-        require("nvim-treesitter.install").update({ with_sync = true })()
-      end,
-          config = function () 
-      local configs = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	build = function()
+		require("nvim-treesitter.install").update({ with_sync = true })()
+	end,
+	config = function()
+		local configs = require("nvim-treesitter.configs")
 
-      configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "asm", "bash", "cpp", "css", "go", "java", "json", "make", "cmake", "python", "rust"},
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },  
-        })
-    end
+		configs.setup({
+			ensure_installed = {
+				"c",
+				"lua",
+				"vim",
+				"vimdoc",
+				"query",
+				"elixir",
+				"heex",
+				"javascript",
+				"html",
+				"asm",
+				"bash",
+				"cpp",
+				"css",
+				"go",
+				"java",
+				"json",
+				"make",
+				"cmake",
+				"python",
+				"rust",
+			},
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
 
 return { M }
